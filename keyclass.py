266b288dd2bc5c21a640424275e460c58481e42d
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-12-12 19:13:31 +0000

    SELF IN INIT

--- keyclass.py
+++ keyclass.py
@@ -24,6 +24,16 @@ keypadPressed = -1
 class Keypad():
 
   def __init__(self, L1, L2, L3, L4, C1, C2, C3, C4):
+    self.L1 = L1
+    self.L2 = L2
+    self.L3 = L3
+    self.L4 = L4
+    self.C1 = C1
+    self.C2 = C2
+    self.C3 = C3
+    self.C4 = C4
+
+    
     # Setup GPIO
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
@@ -126,7 +136,7 @@ class Keypad():
       GPIO.output(line, GPIO.LOW)
 
 
-key = Keypad(L1, L2,L3,L4,C1, C2, C3, C4)
+key = Keypad(L1, L2, L3, L4, C1, C2, C3, C4)
 try:
     while True:
         # If a button was previously pressed,
