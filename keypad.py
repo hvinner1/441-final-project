05e73dac0661912810fa0c86fae7a47c0fb9b754
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-12-12 19:20:41 +0000

    sleep val

--- keypad.py
+++ keypad.py
@@ -21,6 +21,7 @@ C4 = 16
 
 # The GPIO pin of the column of the key that is currently
 # being held down or -1 if no key is pressed
+global keypadPressed
 keypadPressed = -1
 
 
@@ -126,30 +127,29 @@ def readLine(line, characters):
         input = input + characters[3]
     GPIO.output(line, GPIO.LOW)
 
-# def runKey():
-#   keypadPressed = -1
-try:
-    while True:
-        # If a button was previously pressed,
-        # check, whether the user has released it yet
-        if keypadPressed != -1:
-            setAllLines(GPIO.HIGH)
-            if GPIO.input(keypadPressed) == 0:
-                keypadPressed = -1
-            else:
-                time.sleep(0.1)
-        # Otherwise, just read the input
-        else:
-            if not checkSpecialKeys():
-                one= readLine(L1, ["1","2","3","A"])
-                two= readLine(L2, ["4","5","6","B"])
-                three= readLine(L3, ["7","8","9","C"])
-                four= readLine(L4, ["*","0","#","D"])
-                time.sleep(0.1)
-            else:
-                time.sleep(0.1)
-        
-except KeyboardInterrupt:
+def runKey():
+  try:
+      while True:
+          # If a button was previously pressed,
+          # check, whether the user has released it yet
+          if keypadPressed != -1:
+              setAllLines(GPIO.HIGH)
+              if GPIO.input(keypadPressed) == 0:
+                  keypadPressed = -1
+              else:
+                  time.sleep(0.2)
+          # Otherwise, just read the input
+          else:
+              if not checkSpecialKeys():
+                  one= readLine(L1, ["1","2","3","A"])
+                  two= readLine(L2, ["4","5","6","B"])
+                  three= readLine(L3, ["7","8","9","C"])
+                  four= readLine(L4, ["*","0","#","D"])
+                  time.sleep(0.2)
+              else:
+                  time.sleep(0.2)
+          
+  except KeyboardInterrupt:
       print("\nApplication stopped!")
 
-# runKey()
+runKey()
