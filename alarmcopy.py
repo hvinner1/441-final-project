99957a35a4b606c667c301f8cf3d6cac8d96379f
hvinner <91498701+hvinner1@users.noreply.github.com>
2021-12-12 21:47:44 +0000

    comma out

--- alarmcopy.py
+++ alarmcopy.py
@@ -4,7 +4,7 @@ from pir import Pir
 from motor import Motor
 GPIO.setmode(GPIO.BCM)
 import multiprocessing
-global pins, cw, ccw, keypadPressed, cstate,
+global pins, cw, ccw, keypadPressed, cstate
 pins = [18,20,22,24] # controller inputs: in1, in2, in3, in4
 ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
         [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
